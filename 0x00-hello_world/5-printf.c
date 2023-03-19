b1627485ec844c6a268edbbd618924b9cec5ce43
RahmaRubi <rahmaelroby47@gmail.com>
2023-03-19 08:46:00 -0700

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main -A program that print a line using the print function
+ * main :-A program that print a line using the print function
  * Return : 0(success)
  */
 int main(void)
