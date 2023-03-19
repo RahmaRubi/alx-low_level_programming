592e33760c51b02d7df7b7798693e6e963acf632
RahmaRubi <rahmaelroby47@gmail.com>
2023-03-19 10:15:57 -0700

    done

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * main: for sizes of different variables
+ * return- always return 0
+ * return: ending point
+ */
+int main(void)
+{
+printf("Size of a char\: " % d, sizeof(char));
+printf("Size of an int\: " % d, sizeof(int));
+printf("Size of a long int\: " % d, sizeof(long int));
+printf("Size of a long long\: " % d, sizeof(long long));
+printf("Size of a float\: " % d, sizeof(float));
+return (0);
+}
+
