3d1298eab5cf0a0f9505bdaae31fb038777bbe24
RahmaRubi <rahmaelroby47@gmail.com>
2023-03-20 09:58:23 -0700

    done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int intial;
+for (intial = '0' ; intial <= '9' ; intial++)
+{
+putchar(intial);
+}
+putchar('\n');
+return (0);
+}
+
