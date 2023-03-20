eb456db0374e1cd50fd01649aad7011cf610a972
RahmaRubi <rahmaelroby47@gmail.com>
2023-03-20 15:29:09 -0700

    done

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,14 +11,14 @@ int j;
 int k;
 for (i = '0'; i <= '9'; i++)
 {
-for (j =  i + 1; j <='9'; j++)
+for (j =  i + 1; j <= '9'; j++)
 {
-for (k = j +1; k <= '9'; k++)
+for (k = j + 1; k <= '9'; k++)
 {
 putchar(i);
 putchar(j);
 putchar(k);
-if ((i!= '7') && (j!= '8') && (k!= '9'))
+if ((i != '7') || (j != '8') || (k != '9'))
 {
 putchar(',');
 putchar(' ');
@@ -30,5 +30,6 @@ continue;
 }
 }
 }
+putchar('\n');
 return (0);
 }
