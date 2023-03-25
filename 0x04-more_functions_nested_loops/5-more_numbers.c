1c9660745a630e0f19f4d3961410d8be1a41928a
RahmaRubi <rahmaelroby47@gmail.com>
2023-03-24 17:29:55 -0700

    done

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers - print from 0 to 9
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+	int a;
+
+for (i = 0; i <= 9; i++)
+{
+	for (j = 0; j <= 14; j++)
+	{        a = j;
+		if (j > 9)
+		{
+			_putchar('1');
+			a % = 10;
+		}
+		_putchar(a + '0');
+	}
+
+_putchar('\n');
+}
+}
