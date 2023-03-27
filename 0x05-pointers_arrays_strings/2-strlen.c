9f7caa033c8aafc04e7f64340d36a904687d0003
RahmaRubi <rahmaelroby47@gmail.com>
2023-03-27 12:58:35 -0700

    done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - function for length of a string
+ * @*s: passing value of str
+ * Return: return length of the string
+ */
+
+int _strlen(char *s)
+{
+int c;
+c = sizeof(s);
+return (c);
+}
