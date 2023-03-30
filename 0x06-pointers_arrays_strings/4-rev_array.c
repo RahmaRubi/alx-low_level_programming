85b8babc7c326f609b563a57848096a2ef34fa99
RahmaRubi <rahmaelroby47@gmail.com>
2023-03-30 16:48:35 -0700

    done

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,8 +4,8 @@
 
 /**
  * reverse_array - a function for reversing an array
- * @a: array to be reversed 
- * @n: number of elements in the array 
+ * @a: array to be reversed
+ * @n: number of elements in the array
  * Return: nothing just using printf or putchar
  */
 void reverse_array(int *a, int n)
@@ -13,7 +13,7 @@ void reverse_array(int *a, int n)
 int i, j, c;
 
 j = 0;
-for (i = n-1; i >= n/2; i--)
+for (i = n - 1; i >= n / 2; i--)
 {
 	c = a[j];
 	a[j] = a[i];
