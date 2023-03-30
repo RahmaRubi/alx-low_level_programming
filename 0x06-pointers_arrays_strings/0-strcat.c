266c5faa8bf1bd3a9a2eaaba6e65a496cdd28015
RahmaRubi <rahmaelroby47@gmail.com>
2023-03-30 07:45:23 -0700

    done

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/*
+ * _strcat - a function used to append a string to another one
+ * @dest: the first string we want to append src to
+ * @src: the second string we would append
+ * Return: returns dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j, len;
+
+	len = strlen(dest);
+
+	for (i = 0; i < 6; i++)
+	{
+		dest[len + i] = src[i];
+
+	}
+	dest[len + i] = '\0';
+	return (dest);
+
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+the last task before filtration, i can commit my thinking skills and my tecknical potential has been improved alot 
+
