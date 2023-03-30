6f0b6b4647d6070cb5e451ea1ba28296a61e424c
RahmaRubi <rahmaelroby47@gmail.com>
2023-03-30 16:26:06 -0700

    done

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,23 +1,28 @@
 #include "main.h"
-#include <string.h>
+
 /**
- * _strncpy - a function used to append a string to another one
- * @dest: the first string we want to append src to
- * @src: the second string we would append
- * @n: intger variable
- * Return: returns dest
+ * _strncpy - copy spacific number of string
+ * @dest: first input
+ * @src: 2ed input
+ * @n: 3th input
+ *
+ * Return: char type
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+	int j;
 
-
-	for (i = 0; src[i] != '\0' && i < n; i++)
+	for (i = 0; *(src + i) != '\0'; i++)
 	{
-		dest[i] = src[i];
-
 	}
-	dest[i] = '\0';
+	for (j = 0; j < n; j++)
+	{
+		*(dest + j) = *(src + j);
+		if (j < n && j > i)
+		{
+			*(dest + j) = '\0';
+		}
+	}
 	return (dest);
-
 }
