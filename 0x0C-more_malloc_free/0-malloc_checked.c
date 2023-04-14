43f9acffe617f0dab0542085c5678a632f854bcc
RahmaRubi <rahmaelroby47@gmail.com>
2023-04-14 02:05:39 -0700

    done

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc checked - a function exit 98 if fails and a pointer if succeded
+ * @b: unsigned paramter
+ * Return: apointer in success
+ */
+
+
+
+void *malloc_checked(unsigned int b)
+{
+int *x;
+x = malloc(sizeof(b));
+if (x == NULL)
+exit(98);
+else
+return (x);
+
+
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+this my more malloc task
