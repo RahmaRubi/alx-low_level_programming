04b62d4bc171d0d84a08dc28e60c00a5ce81cff3
RahmaRubi <rahmaelroby47@gmail.com>
2023-04-17 08:30:27 -0700

    done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a structure compoed from 3 members
+ * @name: first member
+ * @age: econd
+ * @owner: third one
+ * Description: hello world
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
