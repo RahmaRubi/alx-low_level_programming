0392fcacf99e1be088568025339660187dc060ca
RahmaRubi <rahmaelroby47@gmail.com>
2023-06-11 23:55:25 -0700

    done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - function that prints it's name
+ * @argc: argument that counts the number of arguments
+ * @argv: argument that stores arguments in an array of char
+ * Return: always 0
+ */
+
+int main (int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+printf("%s\n",argv[i]);
+	return (0);
+
+
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+this is a readmea for argc and argv  project 
+- argc is to count number of arguments 
+- argv is an array storing the arguments command line ine of size argc
