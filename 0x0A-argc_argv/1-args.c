3cda910771079f1182b22b9c23ad3ed921140d12
RahmaRubi <rahmaelroby47@gmail.com>
2023-06-12 01:25:30 -0700

    done

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,21 @@
 #include<stdio.h>
 #include<string.h>
 
-int main (int argc, char *argv[])
+/**
+ * main - the main function, mystro one
+ * @argc: counter arogumenter
+ * @argv: array of arguments
+ * Return: returns 0
+ */
+
+int main(int argc, char *argv[])
 {
-	if(argc)
+	if (argc)
 	{
 	printf("%d\n", argc - 1);
 	(void)argv;
 	}
 
-	return(0);
+	return (0);
 }
 
