ff33b9b2863859a188aadbe77e8c2d729b9e743b
RahmaRubi <rahmaelroby47@gmail.com>
2023-06-15 08:02:09 -0700

    done

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * malloc_checked - checking the return value of malloc function
+ * @b: desired allocating size
+ * Return: 98 in failure or pointer in success
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(sizeof(b));
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	else
+		return (p);
+
+
+
+
+}
+
