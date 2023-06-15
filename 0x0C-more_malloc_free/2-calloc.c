44f73f09d99ae87bc42e4d80f68a9b4bbd894f6f
RahmaRubi <rahmaelroby47@gmail.com>
2023-06-15 11:20:39 -0700

    done

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* _calloc - function for creation array of char
+* @nmemb: number of elements
+* @size: intial charcter the pointer points to it
+* Return: return pointer
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+
+	if (size && nmemb)
+	{
+		char *str;
+
+		str = malloc(size * nmemb);
+		if (str)
+		{
+			for (i = 0; i < size; i++)
+			str[i] = 0;
+			return (str);
+		}
+		else
+			return (NULL);                     
+	}
+	return (NULL);
+}
