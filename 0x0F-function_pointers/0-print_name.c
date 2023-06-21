fe58da9533afd89264756b098cba3fe8b89e53f9
RahmaRubi <rahmaelroby47@gmail.com>
2023-06-21 10:18:21 -0700

    done

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - function with callback
+ * @name: first parameter which is pointer
+ * @f: paramter function
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
