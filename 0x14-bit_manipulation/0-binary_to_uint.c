ea92e48dbf98b52683e7c344888b1c6ede4f16d8
RahmaRubi <rahmaelroby47@gmail.com>
2023-07-06 15:01:01 -0700

    done

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int sum, bpow, x;
+	int c, i;
+
+	sum = 0, bpow = 1, c = 0;
+	if (b == NULL || *b == '\0')
+		return (0);
+	else
+	{
+	while (b[c] != '\0')
+		c++;
+	for (i = c - 1; i >= 0; i--)
+	{
+		if ((b[i] ==  '1') || (b[i] == '0'))
+		{
+			x = (b[i] - '0');
+			sum += (bpow * x);
+			bpow *=  2;
+		}
+		else 
+		{	
+			sum = 0;
+			break;
+
+		}
+	
+	}
+		return (sum);
+	}
+
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+Readmea for bit mask
