ad2b59855a314a98aa672d13479a5821a8284697
RahmaRubi <rahmaelroby47@gmail.com>
2023-07-11 10:21:57 -0700

    done

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * read_textfile - read text
+ * @filename: filename
+ * @letters: how many letters should be read
+ * Return: no read charchters
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int r, w;
+	int fp;
+	char *buff;
+
+	if (filename == NULL)
+		return (0);
+	fp = open(filename, O_RDONLY);
+	if (fp < 0)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+	{
+		close(fp);
+		return (0);
+	}
+	r = read(fp, buff, letters);
+	if (r < 0)
+	{
+		close(fp);
+		free(buff);
+		return (0);
+	}
+	w = write(1, buff, r);
+	if (w < 0)
+	{
+	close(fp);
+	free(buff);
+	return (0);
+	}
+	return (r);
+
+
+
+
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+readmea for file input output
