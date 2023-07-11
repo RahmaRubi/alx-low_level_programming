346c6b57dec80afde52ef1c8f9eda7e8d3579957
RahmaRubi <rahmaelroby47@gmail.com>
2023-07-11 11:39:15 -0700

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * create_file - create a file and truncate
- * @filename: file name to be created 
+ * @filename: file name to be created
  * @text_content: content to be added
  * Return: 1 or -1
  */
@@ -13,11 +13,16 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0060);
-	while(text_content && text_content[i] != '\0')
+	if (fp == -1)
+		return (-1);
+	while (text_content && text_content[i] != '\0')
 		i++;
 	w = write(fp, text_content, i);
-	if (fp == -1 || w == -1)
+	if (w == -1)
+	{
+		close (fp);
 		return (-1);
+	}
 	close(fp);
 	return (1);
 
