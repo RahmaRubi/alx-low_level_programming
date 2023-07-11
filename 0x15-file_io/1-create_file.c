961a635f2ee36bcff160c3396cba002d5629a0e7
RahmaRubi <rahmaelroby47@gmail.com>
2023-07-11 11:42:47 -0700

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	i = 0;
 	if (filename == NULL)
 		return (-1);
-	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0060);
+	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fp == -1)
 		return (-1);
 	while (text_content && text_content[i] != '\0')
