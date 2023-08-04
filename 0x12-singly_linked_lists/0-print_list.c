7ed0d033393630c8f20a0de7dd7cd81c4c30bf09
RahmaRubi <rahmaelroby47@gmail.com>
2023-08-04 03:29:11 -0700

    done

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+size_t print_list(const list_t *h)
+{
+	size_t i;
+	i = 0;
+	while(h->next)
+	{
+		if (h->str)
+			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		i++;
+		h = h->next;
+	}
+	return (i);
+
+}
