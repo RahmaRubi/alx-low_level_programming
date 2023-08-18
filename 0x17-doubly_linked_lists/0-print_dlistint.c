08fca25de8eb88f1f6295d098397f58b99d4e283
RahmaRubi <rahmaelroby47@gmail.com>
2023-08-18 01:05:29 -0700

    Done

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,11 +7,11 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i = 0;
-	dlistint_t *temp;
+	const dlistint_t *temp;
 	temp = h;
 	while (temp != NULL)
 	{
-		printf("temp->n\n");
+		printf("%d\n",temp->n);
 		temp = temp->next;
 		i++;
 	}
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -4,10 +4,10 @@
  * @h: head
  * Return: number of nodes
  */
-size_t dlistint_len(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t i = 0;
-	dlistint_t *temp;
+	const dlistint_t *temp;
 	temp = h;
 	while (temp != NULL)
 	{
