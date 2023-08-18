281debaacaafd20a0eafd24e6ea1707057c5c9ef
RahmaRubi <rahmaelroby47@gmail.com>
2023-08-18 09:00:40 -0700

    Done

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,8 +1,27 @@
 #include "lists.h"
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *end;
+	dlistint_t *end, *temp;
+
 	end = malloc(sizeof(dlistint_t));
-	if(!end)
+	if (!end)
 		return (NULL);
+	temp = *head;
+	end->n = n;
+	end->prev = NULL;
+	end->next = NULL;
+	if (*head == NULL)
+	{
+		*head = end;
+		return (*head);
+	}
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = end;
+	end->prev = temp;
+	end->next = NULL;
+	return (*head);
+
 }
